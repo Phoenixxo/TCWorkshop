5f60d48967540ea178edd4a30bcda13e0b9004eb
phoenixxo <caydentisdale43@gmail.com>
2024-09-19 17:36:40 -0400

    WS1 Problem 3

--- workshop-01-javascript/practice-problems/problem-3/main.js
+++ workshop-01-javascript/practice-problems/problem-3/main.js
@@ -1,1 +1,27 @@
 // Fill in for exercises!
+let guess
+let attempts = 5
+
+const guessNumber = (targetNumber) => {
+    while (guess != targetNumber && attempts > 0) {
+        guess = prompt("Guess the number: ")
+
+        if (guess > targetNumber) {
+            attempts = attempts - 1
+            console.log(`attempts left: ${attempts}`);
+            console.log("Too high!");
+        } else if (guess < targetNumber) {
+            attempts = attempts - 1
+            console.log(`attempts left: ${attempts}`);
+            console.log("Too low!");
+        } else {
+            console.log("Correct!");
+        }
+
+        if (attempts <= 0) {
+            console.log("Game over!");
+        }
+    }
+};
+
+guessNumber(30);
