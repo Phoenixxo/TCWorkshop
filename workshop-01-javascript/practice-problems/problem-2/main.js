6a713674ec27d926ec74c609f7c629354f68299f
phoenixxo <caydentisdale43@gmail.com>
2024-09-19 17:11:27 -0400

    WS1 Problem 2

--- workshop-01-javascript/practice-problems/problem-2/main.js
+++ workshop-01-javascript/practice-problems/problem-2/main.js
@@ -1,1 +1,35 @@
 // Fill in for exercises!
+
+const user = {
+    username: "johndoe1",
+    password: "anonymous2",
+    isLoggedIn: false
+};
+
+const login = (username, password) => {
+    if (user.username === username && user.password === password) {
+        user.isLoggedIn = true;
+        return console.log("Login successful!")
+    } else {
+        return console.log("Invalid username or password!")
+    };
+}
+
+const logout = () => {
+    user.isLoggedIn = false
+    return console.log("Logged out!")
+}
+
+const checkLoginStatus = () => {
+    if (user.isLoggedIn === true) {
+        return console.log("Logged in.")
+    } else {
+        return console.log("Logged out.")
+    }
+}
+
+login("johndoe1", "anonymous2")
+login("johndoe2", "anonymous2")
+checkLoginStatus()
+logout()
+checkLoginStatus()
